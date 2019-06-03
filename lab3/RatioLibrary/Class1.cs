aa2faf124f238276488e25e2ed522864b45baa0a
s0ull3ss <14828321+s0ull3ss@users.noreply.github.com>
2019-06-03 19:09:21 +0200

    lab3 init

---
+++ lab3/RatioLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RatioLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ lab3/RatioLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RatioLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RatioLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("269f3560-e89c-495e-b2cb-24a4655d4af9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
